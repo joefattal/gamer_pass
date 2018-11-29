7d972fe83c873839c2e4070621c7182d471935ae
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-11-28 22:47:01 -0700

    final gif addition

---
+++ public/assets/signup-eea5a89d829eed2b68574971ff91e3c83c25a4a799df857aabe2d4aa4d747284.js
@@ -0,0 +1,33 @@
+
+//
+// var modalButton = document.getElementById("signup-button");
+// var modal = document.getElementById("signup-modal");
+// var closeModalButton = document.getElementById("hamburger")
+//
+// function openModal() {
+// 	return modal.style.display = "flex"
+// }
+//
+// function closeModal() {
+// 	return modal.style.display = "none"
+// }
+//
+// modalButton.addEventListener("click", openModal);
+// closeModalButton.addEventListener("click", closeModal);
+
+
+var modalButton = document.getElementById("signup-form-transition");
+var submitButton = document.getElementById("signup-form-submit")
+var basicsDiv = document.getElementById("basic__details");
+var systemsDiv = document.getElementById("game__selection");
+
+// console.log(modalButton, submitButton, basicsDiv, systemsDiv)
+function toggleModal() {
+	basicsDiv.classList.toggle("signup__hidden");
+	systemsDiv.classList.toggle("signup__hidden");
+	modalButton.classList.toggle("signup__hidden");
+	submitButton.classList.toggle("signup__hidden")
+	console.log("modal opening");
+}
+
+modalButton.addEventListener("click", toggleModal);
